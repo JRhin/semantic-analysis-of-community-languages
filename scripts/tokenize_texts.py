8d6335b03d9d1d519d721873712de48f3f0a1995
JRhin <github@jrhin.com>
2024-11-22 02:15:56 +0100

    update added ids to tokens

--- scripts/tokenize_texts.py
+++ scripts/tokenize_texts.py
@@ -22,7 +22,7 @@ def main() -> None:
     """The main loop.
     """
     # Defining paths
-    platform: str = "reddit"
+    platform: str = "paranormal_good"
     CURRENT: Path = Path('.')
     DATA_DIR: Path = CURRENT / 'data'
     PARQUET_PATH: Path = DATA_DIR / f"{platform}.parquet"
@@ -34,21 +34,23 @@ def main() -> None:
     spacy.prefer_gpu()
     
     # Get the languages
-    df = pl.scan_parquet(PARQUET_PATH)
+    df = pl.scan_parquet(PARQUET_PATH).drop_nulls()
     languages = df.select('language').unique().sort('language').collect().get_column('language').to_list()
 
     print(f'Retrieving the texts from {PARQUET_PATH}...')
     corporas = dict()
+    ids = dict()
     for language in tqdm(languages):
         # Get the corporas for each language
-        corporas[language] = df.filter(pl.col('language') == language).select(column).drop_nulls().collect().get_column(column).to_list()
+        corporas[language] = df.filter(pl.col('language') == language).select(column).collect().get_column(column).to_list()
+        ids[language] = df.filter(pl.col("language") == language).select('id').collect().get_column('id').to_list()
 
     print()
     print('Starting tokenizing the texts...')
     nlp = Tokenizer()
     for language in corporas:
         # Perform the texts tokenization
-        tokenized_texts, adjectives = nlp.tokenize(corporas[language], language)#, batch_size=15000)
+        tokenized_texts, adjectives, final_ids = nlp.tokenize(corporas[language], language, ids=ids[language], batch_size=100)
 
         # Define the path where to cache the results
         path: Path = DATA_DIR / f"{platform}"
@@ -58,7 +60,8 @@ def main() -> None:
         pl.DataFrame({'Token': list(adjectives.keys()),
                       'Count': list(adjectives.values())}).write_parquet(path/f'{language.lower()}_adjectives.parquet')
         del adjectives
-        pl.DataFrame({'Texts': tokenized_texts}).write_parquet(path/f'{language.lower()}_tokens.parquet')
+        pl.DataFrame({'id': final_ids,
+                      'Texts': tokenized_texts}).write_parquet(path/f'{language.lower()}_tokens.parquet')
         del tokenized_texts
 
     return None
